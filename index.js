19975d78db7fd13d8aeee2b54b1205259f2fd3f9
manjari jain <manjarijain98@gmail.com>
2020-02-25 05:42:33 +0530

    Install Express and setup its server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const port=8000;
+const app=express();
+
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log(`Error is found on the server ${err}`);
+        return;
+    }
+    console.log(`Server is working fine on port ${port}`);
+})
